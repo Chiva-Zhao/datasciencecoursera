ca33dae6ec5181be9f7c2859b96ff518d6398166
Chiva-Zhao <zhaozhenhua88@gmail.com>
2016-05-06 10:50:22 +0800

    列表与数据框

--- 20160428.R
+++ 20160428.R
@@ -86,3 +86,132 @@ attr(x,"names")
 attr(x,"names") <- c("apple","grapes"); x
 attr(x,"type") <- "fruit"; x
 attributes(x)
+
+# factor()函数
+sex <- c("M","F","M","M", "F")
+sexf <- factor(sex); sexf
+sex.level <- levels(sexf); sex.level
+sex.tab <- table(sexf); sex.tab
+# tapply()
+height <- c(174, 165, 180, 171, 160)
+tapply(height, sex, mean)
+# gl()
+gl(3,5);gl(3,1,15)
+# 多维数组和矩正
+z<-1:12;dim(z)<-c(3,4)
+dim(z)<-12;z
+# array(data = NA, dim = length(data), dimnames = NULL)
+X <- array(1:20,dim=c(4,5));X
+Z <- array(0,dim=c(3, 4, 2));Z
+# matrix(data=NA, nrow=1, ncol=1, byrow=FALSE, dimnames=NULL)
+A<-matrix(1:15, nrow=3,ncol=5,byrow=TRUE);A
+# 数组下标
+A<-matrix(1:15, nrow=3,byrow=TRUE);A<-matrix(1:15, ncol=5,byrow=TRUE)
+a <- 1:24;dim(a) <- c(2,3,4);a[2, 1, 2]
+a[1, 2:3, 2:3];a[1, , ];a[ , 2, ];a[1,1, ]
+# 不规则数组下标
+b <- matrix(c(1,1,1,2,2,3,1,3,4,2,1,4), ncol=3, byrow=T)
+a[b];a[b] <- c(101,102,103,104)
+# 数组四则运算
+A <- matrix(1:6, nrow=2, byrow=T); A
+B <- matrix(1:6, nrow=2); B
+C <- matrix(c(1,2,2,3,3,4), nrow=2); C
+D <- 2*C+A/B; D
+x1 <- c(100,200);x2 <- 1:6;x1+x2
+x3 <- matrix(1:6, nrow=3);x3;x1+x3
+x2 <- 1:5;x1+x2
+# 矩阵的运算
+A<-matrix(1:6,nrow=2); A;t(A)
+det(matrix(1:4, ncol=2))
+x <- 1:5; y <- 2*1:5;x %*% y;crossprod(x,y);
+x %o% y;tcrossprod(x,y);outer(x,y)
+# crossprod(A,B) 等价于t(A) %*% B
+# tcrossprod(A,B) 等价于A %*% t(B) 
+# 矩阵乘法
+A <- array(1:9,dim=(c(3,3)));A
+B <- array(9:1,dim=(c(3,3)));B
+C <- A * B; C
+D <- A %*% B; D
+# 生成对角阵和矩阵取对角运算
+v<-c(1,4,5);diag(v)
+M<-array(1:9,dim=c(3,3));diag(M)
+# 线性方程组和矩阵的逆
+A <- t(array(c(1:8, 10),dim=c(3,3)))
+b <- c(1,1,1)
+x <- solve(A,b); x
+B <- solve(A); B
+# 矩阵的特征值和特征向量
+Sm<-crossprod(A,A)
+ev<-eigen(Sm); ev
+# 矩阵的奇异值分解
+svdA<-svd(A); svdA
+attach(svdA)
+u %*% diag(d) %*% t(v)
+det(A)#行列式
+# 最小拟合和QR分解
+x<-c(0.0, 0.2, 0.4, 0.6, 0.8); y<-c(0.9, 1.9, 2.8, 3.3, 4.2)
+lsfit.sol<-lsfit(x,y)
+lsfit.sol;ls.print(lsfit.sol);ls.diag(lsfit.sol)
+X<-matrix(c(rep(1,5), x), ncol=2);X
+Xplus <- qr(X); Xplus
+b <- qr.coef(Xplus, y); b#系数
+fit <- qr.fitted(Xplus, y); fit#拟合值和残差
+res <- qr.resid(Xplus, y); res
+#矩阵（数组）运算相关函数
+A<-matrix(1:6,nrow=2); A
+dim(A);nrow(A);ncol(A)
+x1 <- rbind(c(1,2), c(3,4)); x1
+x2 <- 10+x1;x2
+x3 <- cbind(x1, x2); x3
+x4 <- rbind(x1, x2); x4
+cbind(1, x1)
+A<-matrix(1:6,nrow=2); A
+X <- matrix(1:6, ncol=2,dimnames=list(c("one","two","three"), c("First","Second")),byrow=T); X
+X<-matrix(1:6, ncol=2, byrow=T)
+dimnames(X) <- list(c("one", "two", "three"), c("First", "Second"))
+colnames(X);rownames(X)
+colnames(X) <- c("First", "Second");rownames(X) <- c("one", "two", "three")
+# 数组的广义转置
+A<-array(1:24, dim = c(2,3,4))
+B<-aperm(A, c(2,3,1));B
+#apply(A, MARGIN, FUN, ...)
+A<-matrix(1:6,nrow=2); A
+apply(A,1,sum);apply(A,2,mean)
+#########################
+#     列表与数据框      #
+#########################
+# Lst <- list(name_1=object_1, ..., name_m=object_m)
+Lst <- list(name="Fred", wife="Mary", no.children=3,child.ages=c(4,7,9))
+Lst[[2]];Lst[[4]][2];Lst[4][[1]][2]
+Lst[["name"]];Lst[["child.ages"]]
+Lst$name;Lst$wife;Lst$child.ages
+Lst$name <- "John"
+Lst$income <- c(1980, 1600);Lst
+list.ABC <- c(list.A, list.B, list.C)
+#data.frame
+df<-data.frame(
+  Name=c("Alice", "Becka", "James", "Jeffrey", "John"),
+  Sex=c("F", "F", "M", "M", "M"),
+  Age=c(13, 13, 12, 13, 12),
+  Height=c(56.5, 65.3, 57.3, 62.5, 59.0),
+  Weight=c(84.0, 98.0, 83.0, 84.0, 99.5)
+); df
+Lst<-list(
+  Name=c("Alice", "Becka", "James", "Jeffrey", "John"),
+  Sex=c("F", "F", "M", "M", "M"),
+  Age=c(13, 13, 12, 13, 12),
+  Height=c(56.5, 65.3, 57.3, 62.5, 59.0),
+  Weight=c(84.0, 98.0, 83.0, 84.0, 99.5)
+); Lst
+as.data.frame(Lst)
+X <- array(1:6, c(2,3));data.frame(X)
+df[1:2, 3:5];df[["Height"]];df$Weight
+names(df);
+rownames(df)<-c("one", "two", "three", "four", "five");df
+# attach detach
+attach(df);r <- Height/Weight; r
+df$r <- Height/Weight
+detach(df)
+#########################
+#     读写数据文件      #
+#########################
